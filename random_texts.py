aac30a0933eb55dfe416ec1c580a0b69ad352816
wojciechguze2 <36681523+wojciechguze2@users.noreply.github.com>
2019-12-12 23:39:39 +0100

    Create random_texts.py

---
+++ random_texts.py
@@ -0,0 +1,14 @@
+en_stories = {
+        1: "Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code readability with its notable use of significant whitespace. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects.",
+        2: "R is a programming language and free software environment for statistical computing and graphics supported by the R Foundation for Statistical Computing.",
+        3: "The South Lake Union Streetcar is a streetcar route in Seattle, Washington, United States. Traveling 1.3 miles (2.1 km), it connects downtown to the South Lake Union neighborhood on Westlake Avenue, Terry Avenue, and Valley Street.",
+        4: "A dictionary is a collection which is unordered, changeable and indexed. In Python dictionaries are written with curly brackets, and they have keys and values.",
+        5: "The comparison operators in and not in check whether a value occurs (does not occur) in a sequence. The operators is and is not compare whether two objects are really the same object; this only matters for mutable objects like lists.",
+        6: "The fortress was never directly attacked during the War and by 1932 much of it had been lost, destroyed by development. Of its original 14,000 feet (4,300 m) of earthworks only 3,000 feet (910 m) remain. The remaining earthworks comprise the Fortress Rosecrans section of the Park. Of the four interior redoubts within Fortress Rosecrans, only Redoubt Brannan survives to this day.",
+        7: "The Old Fort Golf Club was established by the City in 1985 and was designed by Leon Howard and Denis Griffiths",
+        8: "A large eruption occurred at 14:11 on 9 December 2019, which resulted in at least sixteen fatalities, eight missing and thirty-three injured, most receiving severe burns. Forty-seven people were on the island when it erupted, all tour participants or staff.",
+        9: "Initially only implemented client-side in web browsers, JavaScript engines are now embedded in many other types of host software, including server-side in web servers and databases, and in non-web programs such as word processors and PDF software, and in runtime environments that make JavaScript available for writing mobile and desktop applications, including desktop widgets.",
+        10: "Formed mostly as the result of volcanic eruptions from geologic subduction between Sunda Plate and Australian Plate, Java is the 13th largest island in the world and the fifth largest in Indonesia by landmass at about 138,800 square kilometres.",
+        }
+import random
+number_of_story, random_story = random.choice(list(en_stories.items()))
